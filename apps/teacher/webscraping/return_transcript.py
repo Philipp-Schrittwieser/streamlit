8cfd34973d5d0e80dd9f85cfa8d1bc8ed88398a0
Philipp Schrittwieser <office@sprechturbo.at>
2024-11-28 07:06:04 +0100

    w

--- apps/teacher/webscraping/return_transcript.py
+++ apps/teacher/webscraping/return_transcript.py
@@ -6,29 +6,29 @@ import requests
 # Liste der Proxyserver
 proxies = [
     {'http': 'http://14ad64224ed74:821cfdcc1b@91.149.228.224:12323'},
-    {'http': 'http://14ad64224ed74:821cfdcc1b@78.143.227.44:12323'},
-    {'http': 'http://91.149.228.224:12323:14ad64224ed74:821cfdcc1b'},
-    {'http': 'http://78.143.227.44:12323:14ad64224ed74:821cfdcc1b'}
+    {'http': 'http://14ad64224ed74:821cfdcc1b@78.143.227.44:12323'}
 ]
 
+
 def get_video_id_with_proxy(url):
-    """Extrahiert die Video-ID aus einer YouTube-URL unter Verwendung eines Proxys."""
-    
     try:
-        # Wähle zufälligen Proxy
         proxy = random.choice(proxies)
-        response = requests.get(url, proxies=proxy)
+        response = requests.get(url, proxies=proxy, timeout=10)
+        response.raise_for_status()
         final_url = response.url
         query = urlparse(final_url).query
         params = parse_qs(query)
         video_id = params.get('v', [None])[0]
         print(f"Video ID: {video_id}")
         return video_id
-    except Exception as e:
-        print(f"Fehler beim Abrufen der Video-ID: {str(e)}")
-        return None
+    except requests.exceptions.ProxyError:
+        print("Proxy-Verbindungsfehler")
+    except requests.exceptions.Timeout:
+        print("Zeitüberschreitung bei der Anfrage")
+    except requests.exceptions.RequestException as e:
+        print(f"Fehler bei der Anfrage: {str(e)}")
+    return None
     
-
 
 def get_subtitles(video_url):
     """Holt die Untertitel für ein gegebenes YouTube-Video mit Proxy."""
@@ -41,16 +41,16 @@ def get_subtitles(video_url):
     proxy = random.choice(proxies)
     
     try:
-        # Erstelle Sitzung mit Proxy
-        session = requests.Session()
-        session.proxies.update(proxy)
-        
         # Hole Transkript mit Proxy
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["de","en"], proxies=session.proxies)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["de","en"], proxies=proxy)
         return transcript
+    except TranscriptsDisabled:
+        print("Untertitel sind für dieses Video deaktiviert.")
+    except NoTranscriptFound:
+        print("Keine Untertitel in den angegebenen Sprachen gefunden.")
     except Exception as e:
         print(f"Fehler beim Abrufen der Untertitel: {e}")
-        return None
+    return None
 
 def return_transcript(video_url):
     if video_url != "":
