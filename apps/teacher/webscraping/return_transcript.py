86f2cc284c03f80d9826cc814678e317d70fddd2
Philipp Schrittwieser <office@sprechturbo.at>
2024-11-28 07:15:27 +0100

    w

--- apps/teacher/webscraping/return_transcript.py
+++ apps/teacher/webscraping/return_transcript.py
@@ -2,6 +2,17 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse, parse_qs
 import random
 import requests
+import http.cookiejar
+import tempfile
+import os
+
+from youtube_transcript_api import (
+    YouTubeTranscriptApi,
+    TranscriptsDisabled,
+    NoTranscriptFound,
+    NoTranscriptAvailable,
+    VideoUnavailable
+)
 
 # Liste der Proxyserver
 proxies = [
@@ -9,11 +20,24 @@ proxies = [
     {'http': 'http://14ad64224ed74:821cfdcc1b@78.143.227.44:12323'}
 ]
 
+def load_cookies(file_path):
+    cookie_jar = http.cookiejar.MozillaCookieJar(file_path)
+    cookie_jar.load(ignore_discard=True, ignore_expires=True)
+    return cookie_jar
+
+def load_user_agent(file_path):
+    with open(file_path, 'r') as file:
+        return file.read().strip()
+
+# Laden Sie Cookies und User-Agent
+cookies = load_cookies('youtube_config.txt')
+user_agent = load_user_agent('user_agent.txt')
 
 def get_video_id_with_proxy(url):
     try:
         proxy = random.choice(proxies)
-        response = requests.get(url, proxies=proxy, timeout=10)
+        headers = {'User-Agent': user_agent}
+        response = requests.get(url, proxies=proxy, timeout=10, cookies=cookies, headers=headers)
         response.raise_for_status()
         final_url = response.url
         query = urlparse(final_url).query
@@ -28,21 +52,25 @@ def get_video_id_with_proxy(url):
     except requests.exceptions.RequestException as e:
         print(f"Fehler bei der Anfrage: {str(e)}")
     return None
-    
 
 def get_subtitles(video_url):
-    """Holt die Untertitel für ein gegebenes YouTube-Video mit Proxy."""
     video_id = get_video_id_with_proxy(video_url)
     if not video_id:
         print("Ungültige YouTube-URL")
         return None
 
-    # Wähle zufälligen Proxy
     proxy = random.choice(proxies)
     
     try:
-        # Hole Transkript mit Proxy
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["de","en"], proxies=proxy)
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            cookies.save(temp_file.name, ignore_discard=True, ignore_expires=True)
+            temp_file_name = temp_file.name
+
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["de","en"], proxies=proxy, cookies=temp_file_name)
+        
+        # Löschen Sie die temporäre Datei nach Verwendung
+        os.unlink(temp_file_name)
+        
         return transcript
     except TranscriptsDisabled:
         print("Untertitel sind für dieses Video deaktiviert.")
