25216416c5831d35641b9acc9b261b68a53bc924
Philipp Schrittwieser <office@sprechturbo.at>
2024-11-28 20:40:28 +0100

    w

--- probe/return_transcript.py
+++ probe/return_transcript.py
@@ -1,27 +1,27 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-from urllib.parse import urlparse, parse_qs
+# from youtube_transcript_api import YouTubeTranscriptApi
+# from urllib.parse import urlparse, parse_qs
 
-def get_video_id(url):
-    """Extrahiert die Video-ID aus einer YouTube-URL."""
-    query = urlparse(url).query
-    params = parse_qs(query)
-    return params.get('v', [None])[0]
+# def get_video_id(url):
+#     """Extrahiert die Video-ID aus einer YouTube-URL."""
+#     query = urlparse(url).query
+#     params = parse_qs(query)
+#     return params.get('v', [None])[0]
 
-def get_subtitles(video_url, language='de'):
-    """Holt die Untertitel für ein gegebenes YouTube-Video."""
-    video_id = get_video_id(video_url)
-    if not video_id:
-        print("Ungültige YouTube-URL")
-        return None
+# def get_subtitles(video_url, language='de'):
+#     """Holt die Untertitel für ein gegebenes YouTube-Video."""
+#     video_id = get_video_id(video_url)
+#     if not video_id:
+#         print("Ungültige YouTube-URL")
+#         return None
 
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
-        return transcript
-    except Exception as e:
-        print(f"Fehler beim Abrufen der Untertitel: {e}")
-        return None
+#     try:
+#         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
+#         return transcript
+#     except Exception as e:
+#         print(f"Fehler beim Abrufen der Untertitel: {e}")
+#         return None
 
-def return_transcript(video_url, language='de'):
-    subtitles_array = get_subtitles(video_url, language)
-    return ' '.join([subtitle['text'] for subtitle in subtitles_array])
+# def return_transcript(video_url, language='de'):
+#     subtitles_array = get_subtitles(video_url, language)
+#     return ' '.join([subtitle['text'] for subtitle in subtitles_array])
 
